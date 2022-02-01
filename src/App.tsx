9487465cdca61ab5172a599b00634f1889a1944f
Jędrzej Sienkiewicz <jedrzejsienkiewicz93@gmail.com>
2022-02-01 21:33:13 +0100

    Modularization and added readme file

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
+import { CreateReportForm } from "./components/CreateReportForm";
 import { Modal } from "./components/Modal";
+
+const days: string[] = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
 function App() {
   const [isOpened, setIsOpened] = useState(false);
+
+  const toggleOpen = () => setIsOpened((opened) => !opened);
+  const formId = "createReport";
   return (
     <section className="App">
-      <button onClick={() => setIsOpened((opened) => !opened)}>
-        Create Report
-      </button>
-      {isOpened && <Modal action={() => setIsOpened((opened) => !opened)} />}
+      <button onClick={toggleOpen}>Create Report</button>
+      {isOpened && (
+        <Modal onCancel={toggleOpen} title="Create Report" formId={formId}>
+          <CreateReportForm days={days} formId={formId} />
+        </Modal>
+      )}
     </section>
   );
 }
